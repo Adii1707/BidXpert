e08eaa31b9b79bbcc7a9ced026839a189f12f22d
unknown <adityasinghghosh36@gmail.com>
2024-07-12 12:13:09 +0530

    updates

--- Model/connection.js
+++ Model/connection.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 const url = "mongodb://localhost:27017/indusApp";
-
-mongoose.connect(url);
+// mongodb://localhost:27017/
+mongoose.connect(url).catch(err => {
+    if (err.name === 'MongoServerSelectionError') {
+      console.error('Server selection error:', err.message);
+    } else {
+      console.error('Other connection error:', err);
+    }
+  });
+  ;
 
 
 console.log("Successfully connected to mongodb database...");
